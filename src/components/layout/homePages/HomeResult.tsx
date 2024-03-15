05ea4650770d0fc10da1810bc5f193a627a2f266
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-15 23:46:20 +0600

    update

--- src/components/layout/homePages/HomeResult.tsx
+++ src/components/layout/homePages/HomeResult.tsx
@@ -1,22 +1,54 @@
-import scss from './HomeResult.module.scss';
+import scss from "./HomeResult.module.scss";
+import logo from "../../../assets/photos/trust_crypt2 1.png";
+import logo2 from "../../../assets/photos/Country.png";
+import logo3 from "../../../assets/photos/Frame right (photo).png";
 const HomeResult = () => {
-  return (
-    <div className={scss.homeResult}>
-      <div className="container">
-        <div className={scss.content}>
-          <header>
+	return (
+		<div className={scss.homeResult}>
+			<div className="container">
+				<div className={scss.content}>
+					<header>
+						<div className={scss.divContent1}>
+							<img src={logo} alt="logo" />
+							<span>Trustcrypt</span>
+						</div>
+						<nav>
+							<ul>
+								<li>Главная</li>
+								<li>Продукты</li>
+								<li>Блог</li>
+								<li>Контакты</li>
+								<div className={scss.ulDiv2}>
+									<img src={logo2} alt="logo" />
+									<select name="" id="">
+										<option value="RU">RU</option>
+										<option value="RU">ENG</option>
+										<option value="RU">KGZ</option>
+									</select>
+								</div>
+							</ul>
+						</nav>
+					</header>
+					<main>
+						<section className={scss.section1}>
+							<div className={scss.section1ContentDiv}>
+								<h2>
+									Передовая кибербезопасность <br /> для любой компании{" "}
+								</h2>
+								<p>
+									Защитите ваш бизнес от всех видов киберугроз <br /> с помощью
+									решений мирового класса.{" "}
+								</p>
+								<button>Подробнее</button>
+							</div>
+							<img src={logo3} alt="photos" />
+						</section>
+					</main>
+					<footer>footer</footer>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-          </header>
-          <main>
-
-          </main>
-          <footer>
-            
-          </footer>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default HomeResult
+export default HomeResult;
--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,18 +1,16 @@
 import { FC, ReactNode } from "react";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import { store } from "../redux/store";
 interface ReduxProvdersProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
-const ReduxProviders: FC<ReduxProvdersProps> = ({children}) => {
-  return (
-    <>
-    <Provider store={store}>
-      {children}
-    </Provider>
-    </>
-  )
-}
+const ReduxProviders: FC<ReduxProvdersProps> = ({ children }) => {
+	return (
+		<>
+			<Provider store={store}>{children}</Provider>
+		</>
+	);
+};
 
 export default ReduxProviders;
