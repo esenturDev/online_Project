4c0c2f4e3226ccaf783f5e9523b3e239d18b2b30
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-16 23:59:09 +0600

    update

--- src/components/pages/loginPages/LoginPages.tsx
+++ src/components/pages/loginPages/LoginPages.tsx
@@ -1,53 +1,61 @@
-import { useNavigate } from 'react-router';
-import scss from './LoginPages.module.scss';
+import { useNavigate } from "react-router";
+import scss from "./LoginPages.module.scss";
 import { Field, Form, Formik } from "formik";
-import Button, {ButtonProps} from '../../Ul/button/Button';
-import { LoginInputResult } from '../../../utils/LoginValidetes';
-import { usePostLoginUsersMutation } from '../../../redux/api/auth';
+import Button, { ButtonProps } from "../../Ul/button/Button";
+import { LoginInputResult } from "../../../utils/LoginValidetes";
+import { usePostLoginUsersMutation } from "../../../redux/api/auth";
 const LoginPages = () => {
-  const [postLogin] =  usePostLoginUsersMutation();
-  const navigate =  useNavigate();
-  const handleLoginUsers = async (values: string) => {
-    const {email, password} = values;
-    const result = await postLogin({email, password});
-    if(result) {
-      
-      navigate("/home");
-    }
-  }
-  return (
-    <div className={scss.loginPages}>
-      <div className="container">
-        <div className={scss.content}>
-          <Formik initialValues={{email: "", password: ""}}
-          onSubmit={handleLoginUsers}
-          validationSchema={LoginInputResult}
+	const [postLogin] = usePostLoginUsersMutation();
+	const navigate = useNavigate();
+	const handleLoginUsers = async (values: string) => {
+		const { email, password } = values;
+		const response = await postLogin({ email, password });
+		if ("data" in response) {
+			localStorage.setItem("token", response.data.token);
+			localStorage.setItem("isAuthUsers", response.data.token);
+			navigate("/home");
+		} else {
+			console.error("login failed:", response.error);
+		}
+	};
+	const buttonInitialValue: ButtonProps = {
+		type: "submit",
+		variant: "primary",
+		color: "blue",
+		width: "300px",
+	};
+	return (
+		<div className={scss.loginPages}>
+			<div className="container">
+				<div className={scss.content}>
+					<Formik
+						initialValues={{ email: "", password: "" }}
+						onSubmit={handleLoginUsers}
+						validationSchema={LoginInputResult}>
+						{({ errors, touched }) => {
+							return (
+								<Form>
+									<Field
+										id={"email"}
+										name={"email"}
+										type="email"
+										placeholder="Email"
+									/>
+									<Field
+										id={"password"}
+										name={"password"}
+										type="password"
+										placeholder="Password"
+									/>
+									<Button {...buttonInitialValue}>Login</Button>
+								</Form>
+							);
+						}}
+					</Formik>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-          >{({errors, touched}) => {
-            return (
-              <Form>
-                <Field 
-                id={"email"}
-                name={"email"}
-                type="email"
-                placeholder="Email"
-                />
-                <Field 
-                id={"password"}
-                name={"password"}
-                type="password"
-                placeholder="Password"
-                />
-              </Form>
-            )
-          }}
-
-          </Formik>
-          
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default LoginPages
+export default LoginPages;
