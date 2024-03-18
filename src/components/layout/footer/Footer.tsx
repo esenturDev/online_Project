ac1d36465bb7455d3b8b7fbb552c9204c39392a4
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-18 16:25:42 +0600

    Footer components finish

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,14 +1,19 @@
-import scss from './Footer.module.scss';
+import scss from "./Footer.module.scss";
 const Footer = () => {
-  return (
-    <footer>
-      <div className="container">
-        <div className={scss.content}>
-          
-        </div>
-      </div>
-    </footer>
-  )
-}
+	return (
+		<footer>
+			<div className="container">
+				<div className={scss.content}>
+					<p>Copyright 2013-2023 istore.kg все права защищены</p>
+					<p>
+						made in <span>MOORE STUDIO</span>
+					</p>
+					<p>Политика конфиденциальности</p>
+					<p>Пользовательское соглашение</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/components/pages/home/section7/Section7.tsx
+++ src/components/pages/home/section7/Section7.tsx
@@ -32,9 +32,38 @@ const Section7 = () => {
 							<p>Пн - Вс 10:00 - 20:00</p>
 						</div>
 					</div>
-          <div className={scss.section7DivContent2}>
-            
-          </div>
+					<div className={scss.section7DivContent2}>
+						<div className={scss.contents}>
+							<h2>Продукция</h2>
+							<p>Mac</p>
+							<p>iPad</p>
+							<p>iPhone</p>
+							<p>Watch</p>
+							<p>Гаджеты</p>
+						</div>
+						<div className={scss.contents}>
+							<h2>10 лет с Вами!</h2>
+							<p>О нас</p>
+							<p>Обратная связь</p>
+						</div>
+						<div className={scss.contents}>
+							<h2>Контакты</h2>
+							<div className={scss.iconAndtext}>
+								<img src="https://istore.kg/static/img/pin.svg" alt="logo" />
+								<a href="#">пр. Манаса, 40 (пер. ул. Киевская)</a>
+							</div>
+							<div className={scss.iconAndtext}>
+								<img src="https://istore.kg/static/img/phone.svg" alt="logo" />
+								<a href="#">
+									+996 (555) 802 000 <br /> +996 (505) 802 000
+								</a>
+							</div>
+							<div className={scss.iconAndtext}>
+								<img src="https://istore.kg/static/img/mail.svg" alt="logo" />
+								<a href="#">office@istore.kg</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
